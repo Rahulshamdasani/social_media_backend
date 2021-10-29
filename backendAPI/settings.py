0ad796ac605d5ed62d7e6ccdbc7b91b77d9ed773
Shamdasani, Rahul <rshamdas@iu.edu>
2021-10-29 13:19:56 -0400

    idk

--- backendAPI/settings.py
+++ backendAPI/settings.py
@@ -257,7 +257,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR,"static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
