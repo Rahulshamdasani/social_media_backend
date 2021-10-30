f8c58c6b85ea17a169e904ebedc9003eda6d38d6
Shamdasani, Rahul <rshamdas@iu.edu>
2021-10-29 20:02:48 -0400

    Revert "Main branch"
    This reverts commit 8f8fc6731279a9c40794de31c4817a541eb7c661.

--- posts/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Posts
-
-
-class PostsSerializer(serializers.ModelSerializer):
-    # postAuthor = serializers.CharField(source='postAuthor__user', read_only=True)
-    class Meta:
-        model = Posts
-        fields = '__all__'
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,56 @@
+from django.http import Http404
+# from django.shortcuts import render, redirect
+# from django.conf import settings
+# from django.contrib.auth.models import User
+
+# from .forms import ProfileForm
+
+
 from .models import Profile
+# from django.contrib.auth.models import User
+# from accounts import models
+# # Create your views here.
+
+# def profile_detail_view(request, username, *args, **kwargs):
+#     # get the profile for the passed username
+#     qs = Profile.objects.filter(user__email = username)
+#     if not qs.exists():
+#         raise Http404
+#     profile_obj = qs.first()
+#         # is_following = profile_obj in user.following.all()
+#     context = {
+#         "username": username,
+#         "profile": profile_obj
+#     }
+#     return render(request, "profiles/detail.html", context)
+
+
+
+# # View to update profile form
+# def profile_update_view(request, *args, **kwargs):
+#     if not request.user.is_authenticated:
+#         return redirect("/login?next=/profile/update")
+#     my_profile = request.user.profile
+
+#     form = ProfileForm(request.POST, instance = my_profile)
+
+#     if form.is_valid():
+#         profileObj = form.save(commit=False)
+#         first_name = form.cleaned_data.get('first_name')
+#         last_name = form.cleaned_data.get('last_name')
+#         email_address = form.cleaned_data.get('email_address')
+#         my_profile.first_name = first_name
+#         my_profile.last_name = last_name
+#         my_profile.email_address = email_address
+#         my_profile.save()
+
+#     context = {
+#         "form":form,
+#         "btn_label":"Save",
+#         "title":"Update Profile",
+#         'curr_prof':my_profile
+#     }
+#     return render(request, "profiles/form.html", context)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions, status
@@ -21,11 +73,15 @@ class UserProfileView(APIView):
         """
         try:
             data = email
+            print(f'{data=} ')
             wantedProfile = data
+            print(f'{wantedProfile=}')
             
             if Profile.objects.filter(user__email=wantedProfile).exists():
+                print("inside IF******")
                 profile = Profile.objects.filter(user__email=wantedProfile).first()
                 profilejson = ProfileSerializer(profile)
+                print(profilejson)
                 return Response(
                     {'profile': profilejson.data},
                     status=status.HTTP_200_OK
@@ -55,6 +111,7 @@ class UpdateUserDetails(generics.UpdateAPIView):
     serializer_class = ProfileSerializer
 
     def patch(self, request, email = None):
+        print("Email passed is ",email)
         if not email:
             return Response(
 
@@ -70,6 +127,7 @@ class UpdateUserDetails(generics.UpdateAPIView):
                     status=status.HTTP_404_NOT_FOUND
                 )
         currProfile = currProfile.first()
+        print(currProfile)
 
         if currProfile:
             
